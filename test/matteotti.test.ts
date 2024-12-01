24d1dd28acf35c491863c3d4524081174332d68b
Javier Godoy <rotvulpix@gmail.com>
2024-12-01 10:38:58 -0500

    Initial commit

---
+++ test/matteotti.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Matteotti from '../lib/matteotti-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/matteotti-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Matteotti.MatteottiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
